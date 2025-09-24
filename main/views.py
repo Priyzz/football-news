65524f479ac634ee85f4abe8bc7ebfa9a5456918
Priyzz <priyanggazvn@gmail.com>
2025-09-24 16:57:55 +0700

    Tutorial 4

--- main/views.py
+++ main/views.py
@@ -95,23 +95,49 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
-      form = AuthenticationForm(data=request.POST)
-
-      if form.is_valid():
+    if request.method == 'POST':
+        # AuthenticationForm harus dipanggil dengan request + data
+        form = AuthenticationForm(request, data=request.POST)  
+        if form.is_valid():
             user = form.get_user()
             login(request, user)
+
+            # simpan last_login di cookie
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
+        else:
+            # kalau gagal, tampilkan error bawaan form
+            messages.error(request, "Username atau password salah.")
+    else:
+        # untuk GET, cukup panggil form kosong
+        form = AuthenticationForm()
 
-   else:
-      form = AuthenticationForm(request)
-   context = {'form': form}
-   return render(request, 'login.html', context)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+
 
 def logout_user(request):
     logout(request)
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
